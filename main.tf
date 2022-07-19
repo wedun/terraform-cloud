d373bf4330c9d49df11a8864ba94a0dbd5410d64
get <laykomdn@rubius180.rubius.rubius.com>
2022-07-19 21:13:17 +0700

    add tf

---
+++ main.tf
@@ -0,0 +1,66 @@
+terraform {
+  cloud {
+    organization = "laykomdn"
+
+    workspaces {
+      name = "cloud-workspace"
+    }
+  }
+
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
+provider "yandex" {
+  token	    = "AQAEA7qhZT6AAATuwUAv6XmX3UqLq6_g_QNPPQY"
+  cloud_id  = "b1gfmggr757ala32rq97"
+  folder_id = "b1g79ptugpleeoj44c6k"
+  zone      = "ru-central1-a"
+}
+
+resource "yandex_compute_instance" "vm-1" {
+  name = "terraform1"
+
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd89ovh4ticpo40dkbvd"
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet-1.id
+    nat       = true
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file("./id_rsa.pub")}"
+  }
+}
+
+resource "yandex_vpc_network" "network-1" {
+  name = "network1"
+}
+
+resource "yandex_vpc_subnet" "subnet-1" {
+  name           = "subnet1"
+  zone           = "ru-central1-a"
+  network_id     = yandex_vpc_network.network-1.id
+  v4_cidr_blocks = ["192.168.10.0/24"]
+}
+
+output "internal_ip_address_vm_1" {
+  value = yandex_compute_instance.vm-1.network_interface.0.ip_address
+}
+
+output "external_ip_address_vm_1" {
+  value = yandex_compute_instance.vm-1.network_interface.0.nat_ip_address
+}
